80635479ad98dd37dae2c0d50822c1b83bd131da
张宇翔 <zz593141477@gmail.com>
2014-05-21 23:41:44 +0800

    absolute move control

--- Host/debug_dialog.py
+++ Host/debug_dialog.py
@@ -6,6 +6,7 @@ from wx import xrc
 import comm
 import util
 import re
+import conf
 
 class DebugDialog(wx.Dialog):
 
@@ -19,11 +20,19 @@ class DebugDialog(wx.Dialog):
 
         self.createText()
         self.createBtns()
+        self.createSliders()
 
         util.CreateCloseShortcut(self)
         self.Bind(comm.EVT_RECV, self.onRecv)
         self.Bind(wx.EVT_SHOW, self.onShow)
 
+    def createSliders(self):
+        self.slider_x = xrc.XRCCTRL(self.panel, "SliderX")
+        self.slider_y = xrc.XRCCTRL(self.panel, "SliderY")
+        self.Bind(wx.EVT_SCROLL_THUMBRELEASE,
+                  self.onSliderChange, self.slider_x)
+        self.Bind(wx.EVT_SCROLL_THUMBRELEASE,
+                  self.onSliderChange, self.slider_y)
 
     def createText(self):
         self.text_xy = xrc.XRCCTRL(self.panel, "TextXY")
@@ -56,14 +65,21 @@ class DebugDialog(wx.Dialog):
             self.text_xy.SetValue("({},{})".format(coord[0], coord[1]))
             self.text_z.SetValue("({})".format(coord[2]))
 
+    def onSliderChange(self, evt):
+        x, y = self.slider_x.GetValue(), self.slider_y.GetValue()
+        x = x / float(self.slider_x.GetMax()) * conf.MACH_MAX_X
+        y = y / float(self.slider_y.GetMax()) * conf.MACH_MAX_Y
+        print "onSliderChange {} {}".format(x, y)
+        comm.SendAbsoluteXYMove(int(x), int(y))
+
     def onCoordSetClick(self, event):
         _id = event.GetId()
         _param = self.Btns[_id]
         if _param[1] == "cur":
-            match = re.search('X=([-\d]+),Y=([-\d]+)',self.text_xy.GetValue())
+            match = re.search('X=([-\d]+),Y=([-\d]+)', self.text_xy.GetValue())
             if match:
                 c = match.groups()
-                if len(c)==2:
+                if len(c) == 2:
                     comm.SendSetCoordinateCommand(c[0], c[1])
         elif _param[1] == "zero":
             comm.SendSetCoordinateCommand(0, 0)
@@ -79,14 +95,14 @@ class DebugDialog(wx.Dialog):
     def onRMoveClick(self, event):
         _id = event.GetId()
         _param = self.Btns[_id]
-        value = 100
+        value = 50
         comm.SendDebugCommand(_param[1], _param[2] * value)
 
 if __name__ == '__main__':
     app = wx.PySimpleApp()
     app.MainLoop()
     dialog = DebugDialog()
-    comm.Init(dialog)
+    comm.Init(win=dialog)
     dialog.ShowModal()
     # comm.SetCallbackWindow(None)
     comm.Close()
