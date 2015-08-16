7c1b103773a4e5905727f2c45e394b3d091e01f2
zhang <zz593141477@gmail.com>
2015-08-16 11:41:10 +0800

    enable assertion on library functions

--- Project/src/main.c
+++ Project/src/main.c
@@ -115,6 +115,7 @@ void assert_failed(uint8_t* file, uint32_t line)
 { 
 	/* User can add his own implementation to report the file name and line number,
 		 ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
+	ERR_MSG("%s:%d", file, line);
 
 	/* Infinite loop */
 	while (1)
